6e00f618334d91bc8f019268056f6d0f077aa109
Sindhu-Mohan <sindhum.kkl@gmail.com>
2016-11-16 18:44:29 +0530

    Implement the color theme changer option in home page and Make the portfolio pages into responsive

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+// zoom-in and zoom-out
 var section ;
 var factor = 0.8;
 
@@ -34,3 +35,50 @@ function increaseFont() {
 function decreaseFont(){
     setFontSize(factor);
 }
+
+// Theme color change
+
+// Blue Color Theme
+$('.colorgreen').click(function(){
+	  	$('.top-bar').css({"background-color":"#3A9952"});
+	   	$('.breadcrumb').css({"background-color":"#3A9952"});
+	   	$('#logo2').css({"color":"#3A9952"});
+	   	$('h1').css({"color":"#3A9952"});
+	   	$('.update-title').css({"background-color":"#3A9952"});
+	    $('.incfont').css({"background-color":"#275F29"});
+	    $('.incfont').css({"background-image":"white"});
+	  	$('.decfont').css({"background-color":"#275F29"});
+	    $('.decfont').css({"background-image":"white"});
+	    $('.reset').css({"background-color":"#275F29"});
+	    $('.reset').css({"background-image":"white"});
+});
+
+// orange Color Theme
+$('.colororange').click(function(){
+	  	$('.top-bar').css({"background-color":"#D77806"});
+	   	$('.breadcrumb').css({"background-color":"#D77806"});
+	   	$('#logo2').css({"color":"#FF5C01"});
+	   	$('h1').css({"color":"#FF5C01"});
+	   	$('.update-title').css({"background-color":"#D77806"});
+	    $('.incfont').css({"background-color":"#A54A0A"});
+	    $('.incfont').css({"background-image":"white"});
+	  	$('.decfont').css({"background-color":"#A54A0A"});
+	    $('.decfont').css({"background-image":"white"});
+	    $('.reset').css({"background-color":"#A54A0A"});
+	    $('.reset').css({"background-image":"white"});
+});
+
+
+$('.colorblue').click(function(){
+	  	$('.top-bar').css({"background-color":"#4484CE"});
+	   	$('.breadcrumb').css({"background-color":"#4484CE"});
+	   	$('#logo2').css({"color":"#4484CE"});
+	   	$('h1').css({"color":"#4484CE"});
+	   	$('.update-title').css({"background-color":"#4484CE"});
+	    $('.incfont').css({"background-color":"blue"});
+	    $('.incfont').css({"background-image":"white"});
+	  	$('.decfont').css({"background-color":"blue"});
+	    $('.decfont').css({"background-image":"white"});
+	    $('.reset').css({"background-color":"blue"});
+	    $('.reset').css({"background-image":"white"});
+});
